4b9aa9f6ab66f48a782556d4672da27de6aa3f9e
k4sr4 <k4sr4_r@yahoo.com>
2017-03-07 23:45:18 +0000

    Heroku Test

--- spec/controllers/peer_teacher_controller_spec.rb
+++ spec/controllers/peer_teacher_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
+#load 'app/controllers/peer_teacher_controller.rb'
 
-RSpec.describe PeerTeacherController, type: :controller do
-
+RSpec.describe PeerTeacher, type: :model do
+  #pending "add some examples to (or delete) #{__FILE__}"
+    describe '#peerTeacher_params' do
+        it "should be defined" do
+            expect { peerTeacher_params }.not_to raise_error
+        end
+    end
 end
